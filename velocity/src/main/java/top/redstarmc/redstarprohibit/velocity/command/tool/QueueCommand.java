1f678879915cd4fedfad516afba55e3bdf6a3c86
pingguomc <141195321+pingguomc@users.noreply.github.com>
2025-07-10 23:36:32 +0800

    fix: 命令确认功能(1)

--- velocity/src/main/java/top/redstarmc/redstarprohibit/velocity/command/tool/QueueCommand.java
+++ velocity/src/main/java/top/redstarmc/redstarprohibit/velocity/command/tool/QueueCommand.java
@@ -2,6 +2,20 @@ package top.redstarmc.redstarprohibit.velocity.command.tool;
 
 import com.velocitypowered.api.command.CommandSource;
 
-public record QueueCommand(CommandSource source, Runnable runnable) {
+public record QueueCommand(CommandSource source, Runnable runnable, int validDuration, Runnable expireTask) {
+
+
+    public QueueCommand(CommandSource source, Runnable runnable, Runnable expireTask) {
+        this(source, runnable, 10, expireTask);
+    }
+
+
+    public QueueCommand(CommandSource source, Runnable runnable, int validDuration, Runnable expireTask) {
+        this.source = source;
+        this.runnable = runnable;
+        this.validDuration = validDuration;
+        this.expireTask = expireTask;
+    }
+
 
 }

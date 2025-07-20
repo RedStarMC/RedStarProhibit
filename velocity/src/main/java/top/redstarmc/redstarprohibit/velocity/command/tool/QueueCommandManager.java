508063d2eb6a862ae8306b55a39f07cb8866fc65
pingguomc <141195321+pingguomc@users.noreply.github.com>
2025-07-20 18:48:52 +0800

    bug: 未知的错误

--- velocity/src/main/java/top/redstarmc/redstarprohibit/velocity/command/tool/QueueCommandManager.java
+++ velocity/src/main/java/top/redstarmc/redstarprohibit/velocity/command/tool/QueueCommandManager.java
@@ -6,8 +6,8 @@ import top.redstarmc.redstarprohibit.common.api.CommandIntroduce;
 import top.redstarmc.redstarprohibit.common.manager.ServerManager;
 import top.redstarmc.redstarprohibit.velocity.manager.VCServerManager;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 import static net.kyori.adventure.text.Component.text;
 
@@ -17,34 +17,38 @@ public class QueueCommandManager {
     private final Map<CommandSource, QueueCommand> queueCommandMap;
 
     public QueueCommandManager(){
-        this.queueCommandMap = new HashMap<>();
+        this.queueCommandMap = new WeakHashMap<>();
     }
 
     public void addCommand(QueueCommand command){
         CommandSource source = command.source();
 
         this.removeCommand(command.source());
+
         VCServerManager.getManager().debug("添加了一条新命令到命令队列");
         this.queueCommandMap.put(source, command);
+
         command.source().sendMessage(text()
                 .append(CommandIntroduce.getPluginPrefix())
                 .append(text("请在30s内输入",NamedTextColor.AQUA))
                 .append(text("/rsp confirm",NamedTextColor.GOLD))
                 .append(text("来确认你的操作",NamedTextColor.AQUA)));
+
         VCServerManager.getManager().runTaskLater(expireRunnable(command),30L);
     }
 
     public Runnable expireRunnable(QueueCommand command){
-        return () -> {
-            QueueCommand matchingQueueCommand = this.queueCommandMap.get(command.source());
-            if(!command.equals(matchingQueueCommand)){
-                return;
-            }
-//            command.source().sendMessage(text()
-//                    .append(CommandIntroduce.getPluginPrefix())
-//                    .append(text("命令已经过期！", NamedTextColor.RED)));
-//            this.queueCommandMap.remove(command.source());
-        };
+        return null;
+        // () -> {
+//            QueueCommand matchingQueueCommand = this.queueCommandMap.get(command.source());
+//            if(!command.equals(matchingQueueCommand)){
+//                return;
+//            }
+////            command.source().sendMessage(text()
+////                    .append(CommandIntroduce.getPluginPrefix())
+////                    .append(text("命令已经过期！", NamedTextColor.RED)));
+////            this.queueCommandMap.remove(command.source());
+//        };
     }
 
     public boolean runCommand(CommandSource source){
